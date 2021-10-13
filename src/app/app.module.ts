85b31bb85088d043b97bf97328b7a85cf163b551
deepaakhil84 <deepaakhiltvm@gmail.com>
2021-10-14 00:41:36 +0100

    create a form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { OverviewComponent } from './overview/overview.component';
+import { FormComponent } from './form/form.component';
 
 const appRoutes: Routes = [{ path: 'overview', component: OverviewComponent }];
 @NgModule({
-  declarations: [AppComponent, OverviewComponent],
+  declarations: [AppComponent, OverviewComponent, FormComponent],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
