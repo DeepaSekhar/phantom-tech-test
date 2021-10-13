ec1084ee6ae61e941e04cc87c0dc119f661ac80d
deepaakhil84 <deepaakhiltvm@gmail.com>
2021-10-14 00:14:53 +0100

    routing on overview component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './overview/overview.component';
 
+const appRoutes: Routes = [{ path: 'overview', component: OverviewComponent }];
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, OverviewComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
